438c13ae0b952ef238d88ff594ce0a5126fc77c8
Kauan <cvitorcordeiro@gmail.com>
2024-07-19 22:29:32 -0300

    first commit

---
+++ src/main/java/dev/kauan/customer_loan/CustomerLoanApplication.java
@@ -0,0 +1,13 @@
+package dev.kauan.customer_loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kauan/customer_loan/CustomerLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kauan.customer_loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
